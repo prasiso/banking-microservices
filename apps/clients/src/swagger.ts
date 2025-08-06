75523fe1016975b95482794bc02fd3b9af9c2f26
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-08-06 17:59:42 -0300

    feat: add swagger

---
+++ apps/clients/src/swagger.ts
@@ -0,0 +1,13 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../package.json';
+import { INestApplication } from '@nestjs/common';
+
+export const addSwagger = (app: INestApplication<any>) => {
+  const config = new DocumentBuilder()
+    .setTitle('Banking Microservices - Clientes')
+    .setDescription('Micro Serviço responsavél pela parte de cliente')
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+};
